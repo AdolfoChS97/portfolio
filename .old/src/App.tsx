00e8f545f4a914612cc7015e22db537c920b165f
Adolfo Chafardett <adolfo1997a@gmail.com>
2024-02-22 15:44:45 -0400

    feat: added old source code to .old folder

---
+++ .old/src/App.tsx
@@ -0,0 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Nav from './components/dummies/Nav';
+
+
+function App() {
+  return (
+    <>
+      <Nav />
+    </>
+  );
+}
+
+
+export default App
---
+++ .old/src/components/dummies/Nav.tsx
@@ -0,0 +1,22 @@
+import { Navbar } from 'react-bootstrap'
+// import Avatar from './../../assets'
+
+function Nav () {
+    return (
+        <>
+            <Navbar>
+                <Navbar.Brand href="#home">
+                    {/* <Avatar/> */}
+                </Navbar.Brand>
+                {/* <Navbar.Toggle />
+                <Navbar.Collapse className="justify-content-end">
+                    <Navbar.Text>
+                        Signed in as: <a href="#login">Mark Otto</a>
+                    </Navbar.Text>
+                </Navbar.Collapse> */}
+            </Navbar>
+        </>
+    )
+}
+
+export default Nav
